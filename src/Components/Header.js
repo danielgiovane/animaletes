77d6ca3b5fa08063a250ea541930644e8e53408a
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-11-21 15:39:43 -0300

    Criando o header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import { ReactComponent as Dogs } from '../Assets/dogs.svg'
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav>
-        <ul>
-          <Link to='/'>Home</Link>
-          <Link to='/login'>Login / Criar</Link>
-        </ul>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to='/'>
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to='/login'>Login / Criar</Link>
       </nav>
-    </div>
+    </header>
   )
 }
 
