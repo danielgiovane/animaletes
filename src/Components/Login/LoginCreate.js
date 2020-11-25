01aca91eb8c64cb8fbd893e946259231b5b8be52
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-11-25 14:28:01 -0300

    Criando hook customizado useFetch, para lidar com request do site.

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -4,6 +4,8 @@ import Input from "../Forms/Input";
 import useForm from "../../Hooks/useForm";
 import { USER_POST } from "../../api";
 import { UserContext } from "../../UserContext";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 
 const LoginCreate = () => {
   const username = useForm();
@@ -11,6 +13,7 @@ const LoginCreate = () => {
   const password = useForm("password");
 
   const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,9 +23,8 @@ const LoginCreate = () => {
       password: password.value,
     });
 
-    const response = await fetch(url, options);
-    if (!response.ok) throw new Error(response.statusText);
-    userLogin(username.value, password.value);
+    const { response } = await request(url, options);
+    if (response.ok) userLogin(username.value, password.value);
   };
 
   return (
@@ -32,7 +34,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json;
+
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (response.ok === false) throw new Error(json.message);
+    } catch (error) {
+      setError(error.message);
+      json = null;
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return { data, error, loading, request };
+};
+
+export default useFetch;
