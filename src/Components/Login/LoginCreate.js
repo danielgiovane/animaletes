2fad109824e1df911f3d64ad04ac1d6e16e5664f
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-11-25 13:55:58 -0300

    Validando e autenticando usuarios no LoginCreate

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
+import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../UserContext";
 
 const LoginCreate = () => {
-  return (
-    <div>
-      Criar
-    </div>
-  )
-}
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm("password");
 
-export default LoginCreate
+  const { userLogin } = React.useContext(UserContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+
+    const response = await fetch(url, options);
+    if (!response.ok) throw new Error(response.statusText);
+    userLogin(username.value, password.value);
+  };
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title"> Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
+};
+
+export default LoginCreate;
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,21 +1,24 @@
-import React from 'react'
-
-
+import React from "react";
 const types = {
   email: {
-    regex: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: 'Preencha um email valido'
-  }
-}
+    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: "Preencha um email valido",
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      "A senha precisa ter 1 Numero, 1 letra minuscula, 1 letra Maiscula e no minimo 8 caracteres",
+  },
+};
 
 const useForm = (type) => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(null);
 
   const onChange = ({ target }) => {
     if (error) validade(target.value);
     setValue(target.value);
-  }
+  };
 
   const validade = (value) => {
     if (type === false) return true;
@@ -29,7 +32,7 @@ const useForm = (type) => {
       setError(null);
       return true;
     }
-  }
+  };
 
   return {
     value,
@@ -37,8 +40,8 @@ const useForm = (type) => {
     onChange,
     error,
     validade: () => validade(value),
-    onBlur: () => validade(value)
-  }
-}
+    onBlur: () => validade(value),
+  };
+};
 
-export default useForm
+export default useForm;
