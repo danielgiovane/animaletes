bfd9eee506841a52081d45cf2856a06ed1303089
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-11-26 12:30:20 -0300

    Criando uma rota protegida para lidar com dados do usuário.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import { UserStorage } from './UserContext';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
+import ProtectedRouter from "./Components/Helper/ProtectedRouter";
 
 function App() {
   return (
@@ -14,8 +16,9 @@ function App() {
         <UserStorage>
           <Header />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login/*' element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRouter path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
