a0499d8b87915716cada5627fe203b8bddfef434
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-12-18 14:31:44 -0300

    Deletando fotos

---
+++ src/Components/Photo/PhotoDelete.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./PhotoDelete.module.css";
+import { PHOTO_DELETE } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+
+const PhotoDelete = ({ id }) => {
+  const { request, loading } = useFetch();
+  const handleClick = async () => {
+    const confirm = window.confirm("Vôce tem certeza que deseja deletar?");
+    if (confirm) {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = PHOTO_DELETE(id, token);
+      const { response } = await request(url, options);
+      if (response.ok) {
+        window.location.reload();
+      }
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletando...
+        </button>
+      ) : (
+        <button className={styles.delete} onClick={handleClick}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
